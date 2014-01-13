b0c28b43461618ba677ee6a6831e82196aff63c0
Jeremy Green <jeremy@octolabs.com>
2014-01-13 13:52:31 -0600

    Added handling for multiple feeds in one list

--- spec/HandlebaRssSpec.js
+++ spec/HandlebaRssSpec.js
@@ -10,10 +10,10 @@ describe("HandlebaRss", function(){
 
   it("should execute a callback after retrieving a feedUrl",function(){
     spyOn($, "ajax").and.callFake(function(options) {
-        options.success();
+        options.success(mockResponse);
     });
     var callback = jasmine.createSpy('callback');
-    rss.retrieveUrl(callback);
+    rss.retrieveUrl(feedUrl,callback);
     expect(callback).toHaveBeenCalled();
   });
 
@@ -32,3 +32,45 @@ describe("HandlebaRss", function(){
   });
 
 });
+
+
+
+describe("HandlebaRss with multiple feeds", function(){
+  var rss;
+  var feedUrls = [
+    "http://www.octolabs.com/blogs/octoblog/feed.xml", 
+    "http://datachomp.com/atom.xml", 
+    "http://geekindulgence.com/feed/"
+  ];
+  var template = '#multi-feed-template';
+  var destination = '#multiple-destination';
+  var limit = 3;
+
+  beforeEach(function(){
+    rss = new HandlebaRss(feedUrls,template,destination,limit);
+    var responses = [mockResponse,dataChompMock,geekIndulgenceMock];
+    var currentResponse = 0;
+    spyOn($, "ajax").and.callFake(function(options) {
+        options.success(responses[currentResponse]);
+        currentResponse++;
+    });
+    rss.init();
+  });
+
+  it("should retrieve the feeds, sort the articles, and render the template",function(){
+    
+
+    
+
+    expect($(destination).text()).toMatch(/Techlahoma - Omniauth Local Setup/);
+    expect($(destination).text()).toMatch(/I am A bad blogger/);
+    expect($(destination).text()).toMatch(/Blogging Goals for 2014/);
+
+    expect($(destination).text()).toMatch(/OctoBlog/);
+    expect($(destination).text()).toMatch(/DataChomp/);
+    expect($(destination).text()).toMatch(/Geek Induldence/);
+
+  });
+
+});
+
--- src/HandlebaRss.js
+++ src/HandlebaRss.js
@@ -1,17 +1,35 @@
-function HandlebaRss(feedUrl,template,destination){
-  this.feedUrl = feedUrl;
+function HandlebaRss(feedUrl,template,destination,multiLimit){
+  if(feedUrl instanceof Array){
+    this.feedUrls = feedUrl;
+  }else{
+    this.feedUrl = feedUrl;
+  }
   this.template = template;
   this.destination = destination;
+  this.multiLimit = multiLimit;
   //this.retrieveUrl();
 }
 
 HandlebaRss.prototype.init = function(){
-  this.retrieveUrl( $.proxy(this.captureAndRender,this) );
+  if(this.feedUrl){
+    this.retrieveUrl(this.feedUrl, $.proxy(this.captureAndRender,this));
+  }else{
+    for(var i = 0; i<this.feedUrls.length; i++){
+      this.retrieveUrl(this.feedUrls[i], $.proxy(this.captureMultiAndRender,this));
+    }
+  }
 }
 
-HandlebaRss.prototype.retrieveUrl = function(callback){
+HandlebaRss.prototype.addFeedData = function(data){
+  if(!this.feedDatas){
+    this.feedDatas = [];
+  }
+  this.feedDatas.push(data.responseData.feed);
+}
+
+HandlebaRss.prototype.retrieveUrl = function(url,callback){
   $.ajax({
-    url: 'http:' + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(this.feedUrl),
+    url: 'http:' + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
     //url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(this.feedUrl),
     dataType: 'json',
     success: callback
@@ -19,9 +37,13 @@ HandlebaRss.prototype.retrieveUrl = function(callback){
 };
 
 HandlebaRss.prototype.captureAndRender = function(data){
-  console.log("calling captureAndRender");
   this.captureFeed(data);
   this.render();
+};
+
+HandlebaRss.prototype.captureMultiAndRender = function(data){
+  this.addFeedData(data);
+  this.renderMulti();
 };
 
 HandlebaRss.prototype.captureFeed = function(data){
@@ -34,3 +56,49 @@ HandlebaRss.prototype.render = function(){
   var html    = template(this.feed);
   $(this.destination).html(html);
 };
+
+
+HandlebaRss.prototype.renderMulti = function(){
+  var source   = $(this.template).html();
+  var template = Handlebars.compile(source);
+  var entries = this.coalesseFeeds();
+  var context = { entries : entries };
+  
+  var html    = template(context);
+  $(this.destination).html(html);
+};
+
+HandlebaRss.prototype.coalesseFeeds = function(){
+  var entries = [];
+  for(var i = 0; i<this.feedDatas.length; i++){
+    var feedData = this.feedDatas[i];
+    // build a representation of the main feed data, which will be added
+    // to each entry
+    var feed = {
+      author : feedData.author,
+      description : feedData.description,
+      feedUrl : feedData.feedUrl,
+      link : feedData.link,
+      title : feedData.title,
+      type : feedData.type
+    }
+    for(var j = 0; j<feedData.entries.length; j++){
+      if(this.multiLimit && j >= this.multiLimit){ break; }
+      var entry = feedData.entries[j];
+      entry.feed = feed;
+      entries.push(entry);
+    }
+  }
+  var sortedEntries = entries.sort(function(a,b){
+    var aDate = Date.parse(a.publishedDate);
+    var bDate = Date.parse(b.publishedDate);
+    if( aDate == bDate){
+      return 0;
+    }else if( aDate < bDate ){
+      return 1;
+    }
+    return -1;
+  });
+  return sortedEntries;
+}
+
